0d231843710c45459993e6f0e674e3e833b8aac2
mdsajib50 <mdnurulalamsiddiki9@gmail.com>
2025-11-19 08:34:37 +0600

    render snake

--- script.js
+++ script.js
@@ -25,3 +25,9 @@ for (let row = 0; row < rows; row++) {
     
 }
 
+function render() {
+    snake.forEach(segment => {
+        blocks[`${segment.x},${segment.y}`].classList.add('filled');
+    });
+}
+render();
